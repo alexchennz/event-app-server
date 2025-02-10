ac4b0ce75fd7ca30745250cb0883b13edd704bd0
Alex Chen <newzealandbuy@gmail.com>
2025-02-11 00:23:00 +1300

    update

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Event = require('../models/Event');
 
 // Connect to MongoDB - removed deprecated options
-mongoose.connect("mongodb+srv://newzealandbuy:bI4Nsm9kQGuNVHEo@eventappdb.vxkgr.mongodb.net/?retryWrites=true&w=majority&appName=eventappdb")
+const uri = `mongodb+srv://${process.env.DB_USER}:` +
+            `${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_URL}/` +
+            `?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`;
+
+// Connect to MongoDB - removed deprecated options
+mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
